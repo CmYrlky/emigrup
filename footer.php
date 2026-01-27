119181462c9cfc2e367a77519e47896469ebe6b9
DokuNoOji <cm.yrlky@gmail.com>
2026-01-27 22:26:37 +0300

    27/01/2026

---
+++ footer.php
@@ -0,0 +1,90 @@
+  <footer id="footer" class="footer dark-background">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row gy-4">
+          <div class="col-lg-4 col-md-6 footer-about">
+            <a href="index.php" class="logo d-flex align-items-center">
+              <span class="sitename">EMİ Grup Bilgi Teknolojileri</span>
+            </a>
+            <div class="footer-contact pt-3">
+              <p>Altunizade Mahallesi Tophanelioğlu Caddesi</p>
+              <p>No:6/2 Kotil İş Merkezi  Üsküdar / İSTANBUL</p>
+              <p class="mt-3"><strong>Tel:</strong> <span>+90 (216) 364 1 364</span></p>
+              <p><strong>Email:</strong> <span>info@emigrup.com.tr</span></p>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Kurumsal</h4>
+            <ul>
+              <li><a href="#">Hakkımızda</a></li>
+              <li><a href="#">Çözüm Ortaklarımız</a></li>
+              <li><a href="#">Referanslarımız</a></li>
+              <li><a href="#">Entegre Kalite Yönetim Politikası</a></li>
+              <li><a href="#">Bilgi Toplumu Hizmetleri</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Çözümler</h4>
+            <ul>
+              <li><a href="#">İletim Hatlarının Bakımı</a></li>
+              <li><a href="#">Dağıtım Hatlarının Bakımı</a></li>
+              <li><a href="#">Drone</a></li>
+              <li><a href="#">Fotogrametri</a></li>
+              <li><a href="#">Lidar</a></li>
+              <li><a href="#">Jeofizik</a></li>
+              <li><a href="#">Havacılık</a></li>
+              <li><a href="#">İmar / Kadastro / Kamulaştırma</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Projeler</h4>
+            <ul>
+              <li><a href="#">Devam Eden Projeler</a></li>
+              <li><a href="#">Tamamlanmış Projeler</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>İletişim</h4>
+            <ul>
+              <li><a href="#">Merkez Ofis</a></li>
+              <li><a href="#">Ankara Şube</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="copyright text-center">
+      <div
+        class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+        <div class="d-flex flex-column align-items-center align-items-lg-start">
+          <!-- <div>
+            2026 <strong><span>MyWebsite</span></strong>. All Rights Reserved
+          </div> -->
+          <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
+            Tasarım <a href="https://wa.me/+905425180948">Cem Yerlikaya</a>
+          </div>
+        </div>
+
+        <!-- <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+          <a href=""><i class="bi bi-twitter-x"></i></a>
+          <a href=""><i class="bi bi-facebook"></i></a>
+          <a href=""><i class="bi bi-instagram"></i></a>
+          <a href=""><i class="bi bi-linkedin"></i></a>
+        </div> -->
+
+      </div>
+    </div>
+
+  </footer>
