003d06e2bbaba291bf58c765f90d3c8dffe6167d
CmYrlky <cm.yrlky@gmail.com>
2026-02-05 15:16:34 +0300

    img

--- carousel.php
+++ carousel.php
@@ -1,54 +1,69 @@
-    <!-- Hero Section -->
-    <section id="hero" class="hero section dark-background">
+<!-- Hero Section -->
+<section id="hero" class="hero section dark-background">
 
-      <div id="hero-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000" >
-        <div class="carousel-item active">      
-          <img src="assets/img/hero_1.jpg" alt="">
-          <div class="carousel-container">
-            <h2>Enerji Nakil Hatlarında Canlı Bakım</h2>
-            <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
-          </div>
-        </div><!-- End Carousel Item -->
-
-        <div class="carousel-item">
-          <video src="assets/img/hero1_video.mp4" autoplay="true" height="100%" width="100%"></video>
-          <!-- <img src="assets/img/hero_2.jpg" alt=""> -->
-           <!-- <video src="assets/img/hero_video.mp4" style="height: 100%; width: 100%;" autoplay></video> -->
-          <div class="carousel-container">
-            <h2>Enerji İletim Hatlarında İzolatör Yıkama</h2>
-            <!-- <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</p> -->
-          </div>
-        </div><!-- End Carousel Item -->
-
-        <div class="carousel-item">
-          <img src="assets/img/hero_3.jpg" alt="">
-          <div class="carousel-container">
-            <h2>Enerji İletim Hatlarında Bakım Çalışmaları</h2>
-            
-          </div>
-        </div>
-
-        <div class="carousel-item">
-          <video src="assets/img/hero2_video.mp4" autoplay="true" height="100%" width="100%"></video>
-          <div class="carousel-container">
-            <h2>Enerji İletim Hatlarında İzolatör Yıkama</h2>
-            
-          </div>
-        </div>
-
-        <div class="carousel-item">
-          <img src="assets/img/hero_5.jpg" alt="">
-          <div class="carousel-container">
-            <h2>Hava Lidar ve Fotogrametri</h2>
-            
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
-        </a>
-        <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
-          <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
-        </a>
-        <ol class="carousel-indicators"></ol>
+  <div id="hero-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+    <div class="carousel-item active">
+      <img src="assets/img/hero_1.jpg" alt="">
+      <div class="carousel-container">
+        <h2>Enerji Nakil Hatlarında Canlı Bakım</h2>
+        <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> -->
       </div>
-    </section><!-- /Hero Section -->
+    </div><!-- End Carousel Item -->
+
+    <div class="carousel-item">
+      <video src="assets/img/hero1_video.mp4" autoplay="true" height="100%" width="100%"></video>
+      <!-- <img src="assets/img/hero_2.jpg" alt=""> -->
+      <!-- <video src="assets/img/hero_video.mp4" style="height: 100%; width: 100%;" autoplay></video> -->
+      <div class="carousel-container">
+        <h2>Enerji İletim Hatlarında İzolatör Yıkama</h2>
+        <!-- <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.</p> -->
+      </div>
+    </div><!-- End Carousel Item -->
+
+    <div class="carousel-item">
+      <video src="assets/img/hero2_video.mp4" autoplay="true" height="100%" width="100%"></video>
+      <div class="carousel-container">
+        <h2>Enerji İletim Hatlarında İzolatör Yıkama</h2>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img src="assets/img/hero_2.jpg" alt="">
+      <div class="carousel-container">
+        <h2>Enerji İletim Hatlarında Bakım Çalışmaları</h2>
+
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img src="assets/img/hero_3.jpg" alt="">
+      <div class="carousel-container">
+        <h2>Hava Lidar ve Fotogrametri</h2>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <video src="assets/img/hero3_video.mp4" autoplay="true" height="100%" width="100%"></video>
+      <div class="carousel-container">
+        <h2>Enerji İletim Hatlarında İzolatör Yıkama</h2>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img src="assets/img/hero_4.jpg" alt="">
+      <div class="carousel-container">
+        <h2>Hava Lidar ve Fotogrametri</h2>
+      </div>
+    </div>
+
+
+
+    <!-- <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+    </a>
+    <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
+      <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+    </a>
+    <ol class="carousel-indicators"></ol> -->
+  </div>
+</section><!-- /Hero Section -->
